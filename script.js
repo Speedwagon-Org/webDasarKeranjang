b3545d2d826404cafd8ac1139c96c9ac70b5def7
HperM- <48154253+TitoRahman@users.noreply.github.com>
2022-01-01 11:12:56 +0700

    Added Plus and Add Button

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ let shops = []
 let count = 1
 let totalPrices = document.getElementById('total-price');
 let totalInCart = document.getElementById('total-item-in-cart');
+
 for (let i = 1; i <= 5; i++) {
     shops.push({
+        shopID: i,
         shopName: document.getElementById('shop-name-' + String(i)).innerHTML,
         shopCheck: document.getElementById('select-all-item-in-shop-' + String(i)),
         shopItems: [],
@@ -12,42 +14,98 @@ for (let i = 1; i <= 5; i++) {
     });
     for (let j = 1; j <= 2; j++) {
         shops[i - 1].shopItems.push({
+            itemID : count-1,
             itemName: document.getElementById('item-title-' + String(count)).innerHTML,
             itemPrice: Number(document.getElementById('item-price-' + String(count)).innerHTML),
             itemChecked: document.getElementById('item-' + String(count) + '-check'),
             itemDeleted: false,
-            itemVisibility: document.getElementById('item-' + String(count)).style
+            itemVisibility: document.getElementById('item-' + String(count)).style,
+            itemQuantity:  Number(document.getElementById('item-quantity-value-' + String(count)).value)
         });
         count += 1;
         totalInCart.innerHTML = String(Number(totalInCart.innerHTML) + 1)
     }
 }
 
+localStorage.setItem('Store Cart', JSON.stringify(shops))
+
+function addQuantity(item)
+{
+    for (let i = 0; i < 5; i++) {
+        for (let j = 0; j < 2; j++) {
+            if(shops[i].shopItems[j].itemID == item-1)
+            {
+                shops[i].shopItems[j].itemQuantity = Number(shops[i].shopItems[j].itemQuantity) + 1
+                document.getElementById('item-quantity-value-' + String(item)).value =  String(shops[i].shopItems[j].itemQuantity)
+            }
+        }
+    }
+    selectItem()
+}
+
+function decQuantity(item)
+{
+    for (let i = 0; i < 5; i++) {
+        for (let j = 0; j < 2; j++) {
+            if(shops[i].shopItems[j].itemID == item-1)
+            {
+                if (shops[i].shopItems[j].itemQuantity > 1)
+                {
+                    shops[i].shopItems[j].itemQuantity = Number(shops[i].shopItems[j].itemQuantity) - 1
+                    document.getElementById('item-quantity-value-' + String(item)).value =  String(shops[i].shopItems[j].itemQuantity)
+                }
+            }
+        }
+    }
+    selectItem()
+}
+
+function changeQuantity(item)
+{
+    for (let i = 0; i < 5; i++) {
+        for (let j = 0; j < 2; j++) {
+            if(shops[i].shopItems[j].itemID == item-1)
+            {
+                if (Number(document.getElementById('item-quantity-value-' + String(item)).value) > 1)
+                {
+                    shops[i].shopItems[j].itemQuantity = Number(document.getElementById('item-quantity-value-' + String(item)).value)
+                }
+                else 
+                {
+                    shops[i].shopItems[j].itemQuantity = 1
+                }
+                //console.log('Item on index ',i,j,'has been changed! to ',document.getElementById('item-quantity-value-' + String(item)).value)
+            }
+        }
+    }
+    selectItem()
+}
+
 function deleteItem(itemDeleted = 0) {
-    let tmp = 0
+    itemDeleted -= 1
     for (let i = 0; i < shops.length; i++) {
         for (let j = 0; j < shops[i].shopItems.length; j++) {
-            if (tmp + 1 == itemDeleted) {
-                shops[i].shopItems[j].itemDeleted = true;
+            if (shops[i].shopItems[j].itemID == itemDeleted) {
+                //shops[i].shopItems[j].itemDeleted = true;
                 shops[i].shopItems[j].itemVisibility.display = 'none';
-                shops[i].shopItems[j].itemChecked.checked = false;
+                //shops[i].shopItems[j].itemChecked.checked = false;
+                shops[i].shopItems.splice(j,1)
+                console.log(i, j)
+                //localStorage.setItem('Store Cart', JSON.stringify(shops))
             }
-            tmp++;
         }
     }
 
     for (let i = 0; i < shops.length; i++) {
-        let countDeletedItem = 0
-        for (let j = 0; j < shops[i].shopItems.length; j++) {
-            if (shops[i].shopItems[j].itemDeleted == true) {
-                countDeletedItem++;
-            }
-        }
-        if (countDeletedItem == 2) {
+        if (shops[i].shopItems.length == 0) {
             shops[i].shopVisibility.display = 'none';
+            console.log('Shops of index ' + String(i) + ' has been deleted')
+            shops.splice(i,1);
+            //localStorage.setItem('Store Cart', JSON.stringify(shops))
         }
     }
     totalInCart.innerHTML = String(Number(totalInCart.innerHTML) - 1)
+    localStorage.setItem('Store Cart', JSON.stringify(shops))
     selectItem()
 }
 
@@ -98,11 +156,11 @@ function selectItem(selectedItem = "Other", selectedShop = "") // Memilih Item
     for (let i = 0; i < shops.length; i++) {
         for (let j = 0; j < shops[i].shopItems.length; j++) {
             if (shops[i].shopItems[j].itemDeleted == false && shops[i].shopItems[j].itemChecked.checked == true) {
-                total += shops[i].shopItems[j].itemPrice;
+                total += (shops[i].shopItems[j].itemPrice * shops[i].shopItems[j].itemQuantity);
 
                 // Create Detail Checkout List
                 let li = document.createElement('li');
-                li.appendChild(document.createTextNode(String(shops[i].shopItems[j].itemName) + " - " + String(shops[i].shopItems[j].itemPrice)));
+                li.appendChild(document.createTextNode(String(shops[i].shopItems[j].itemName) + " - " + String(shops[i].shopItems[j].itemPrice) + " x " + String(shops[i].shopItems[j].itemQuantity)));
                 listItemSelected.appendChild(li);
             }
         }
